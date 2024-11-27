f7ad5cd517f52408fee096da76bcec4ccd194da7
moniskhantelus <93679325+moniskhantelus@users.noreply.github.com>
2024-11-27 01:04:30 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "policy" {
-  description = "Resource snapshot policy details."
-  value       = google_compute_resource_policy.policy
-}
 
 output "attachments" {
   description = "Disk attachments to the resource policy."
